458efd21d2ee6c5c57548066410fe09904e23d6e
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 16:01:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(katz_deli, person)
   katz_deli.push(person)
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
