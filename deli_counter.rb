837d393f7787a08ff95ce211556a9dfc923ab636
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 15:49:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else
-    katz_deli.each do |person|
-      katz_deli[katz_deli.index(person)] = katz_deli.index(person) + 1 + ". " + katz_deli[katz_deli.index(person)]
-    end
+  # else
+  #   katz_deli.each do |person|
+  #     katz_deli[katz_deli.index(person)] = katz_deli.index(person) + 1 + ". " + katz_deli[katz_deli.index(person)]
+  #   end
   end
 
   puts katz_deli.join(" ")
